0dd681281d7f04d7f2f5d585cc8cfae724a9b0bc
Hristina20299 <hristina.velkova.27@gmail.com>
2020-10-14 20:40:06 +0000

    ----------

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -6,11 +6,13 @@ public class NumberSearch {
   public static void main(String[] args) {
     int count = 0;
     for (int number = 100; number <= 1000; number++) {
-      if (number % 5 == 0 && number % 6 == 0) {
+      if(number % 5 == 0 && number % 6 == 0) {
         System.out.print(number + " ");
-        if (number % 100 == 0) {
-          System.out.println(" ");
-        }
+        count++;
+      }
+      if (count==10) {
+        System.out.println();
+        count = 0;
       }
     
 
