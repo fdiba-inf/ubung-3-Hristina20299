0dd8118fc94d7cc7964c3c39db21a2c6b65332b9
Hristina20299 <hristina.velkova.27@gmail.com>
2020-10-15 11:57:36 +0000

    ~~~~~

---
+++ src/exercise3/AngleCalculation.java
@@ -0,0 +1,24 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class AngleCalculation {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double value = input.nextDouble();
+    char type = input.next().charAt(0);
+     do {
+       if (type == 'r') {
+        double grad = (180.0 * value) / Math.PI;
+        System.out.println("Angle: " + grad + 'd');
+        break;
+       } else {
+         if (type == 'd');
+         double radiant = (value * Math.PI) / 180.0;
+         System.out.println("Angle: " + radiant + 'r');
+         break;
+       }
+      
+    } while (type == 'r' || type == 'd');
+  } 
+}
