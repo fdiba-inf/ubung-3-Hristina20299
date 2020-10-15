21bec99e110896af0691bb064b2716a66b0ac14f
Hristina20299 <hristina.velkova.27@gmail.com>
2020-10-15 13:50:58 +0000

    .......

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -9,25 +9,30 @@ public class Triangle {
     double b = input.nextDouble();
     double c = input.nextDouble();
     do {
-      if (a == b && b == c) {
-        double s = (a + b + c) / 2;
-        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        System.out.println("Area: " + area);
-        System.out.println("Triangle: equilateral");
-      } else if (a == b || b == c) {
-        double s = (a + b + c) / 2;
-        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        System.out.println("Area: " + area);
-        System.out.println("Triangle: scalene");
-      } else if {
-        double s = (a + b + c) / 2;
-        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-        System.out.println("Area: " + area);
-        System.out.println("Triangle: isosceles");
+      if (a < (b + c) && b < (a + c) && c < (a + b)) {
+        if (a == b && b == c && a == c) {
+          double s = (a + b + c)/2;
+          double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+          System.out.println("Area: " + area);
+          System.out.println("Triangle: equilateral");
+        } else if (a == b || b == c || c == a) {
+            double s = (a + b + c)/2;
+            double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+            System.out.println("Area: " + area);
+            System.out.println("Triangle: isosceles");
+          } else {
+            double s = (a + b + c)/2;
+            double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+            System.out.println("Area: " + area);
+            System.out.println("Triangle: scalene");
+          }
+
       } else {
         System.out.println("Values are not correct!");
       }
-
+      a = input.nextDouble();
+      b = input.nextDouble();
+      c = input.nextDouble();
     } while (a > 0 && b > 0 && c > 0);
   }
 }
